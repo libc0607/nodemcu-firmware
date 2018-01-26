280e422877a6d1922357767e6ee580afe42a1843
libc0607 <libc0607@gmail.com>
2018-01-26 11:35:47 +0800

    modules/rtl8370.c: add vlan bindings.

--- app/modules/rtl8370.c
+++ app/modules/rtl8370.c
@@ -49,7 +49,7 @@ static int rtl8370_smi(lua_State* L)
 {
 	uint8_t pin_SDA = luaL_checkinteger(L, 1);
 	uint8_t pin_SCK = luaL_checkinteger(L, 2);
-  //uint8_t pin_RST = luaL_checkinteger(L, 3);
+
 	uint32_t ret;
 
 	MOD_CHECK_ID( gpio, pin_SDA );
@@ -71,6 +71,17 @@ static int rtl8370_hardreset(lua_State* L)
 	MOD_CHECK_ID( gpio, pin_RST );
 	
 	ret = smi_reset(pin_RST);
+	
+	lua_pushnumber(L, ret);
+	return 1;
+}
+
+// Lua: status = rtl8370.softreset()
+static int rtl8370_softreset(lua_State* L)
+{
+	uint32_t ret;
+	
+	ret = rtl8370_setAsicReg(RTL8370_REG_CHIP_RESET, (1<<RTL8370_CHIP_RST_OFFSET));
 	
 	lua_pushnumber(L, ret);
 	return 1;
@@ -281,7 +292,7 @@ static int rtl8370_led_serialMode(lua_State* L)
 
 //=================== vlan ============================
 
-// Lua: status = rtl8370.vlan.init()
+// Lua: status = rtl8370.vlan_init()
 static int rtl8370_vlan_init(lua_State* L)
 {
 	rtk_api_ret_t ret;
@@ -292,24 +303,293 @@ static int rtl8370_vlan_init(lua_State* L)
 	return 1;
 }
 
-// Lua: status[, mode] = rtl8370.vlan(vid, member_mask, )
+// Lua: status[, member_mask, untag_mask, fid] = rtl8370.vlan(vid[, member_mask, untag_mask, fid])
 static int rtl8370_vlan(lua_State* L)
 {
 	uint32_t argc = lua_gettop(L);
 	rtk_api_ret_t ret;
-	rtk_led_active_t active;
+	rtk_vlan_t vid;
+	rtk_portmask_t member_mask;
+	rtk_portmask_t untag_mask;
+	rtk_fid_t fid;
 
-	if (argc == 0) {
+	if (argc == 1) {
 		// get
-		ret = rtk_led_serialMode_get((rtk_data_t *)(&active));
+		vid = luaL_checkinteger(L, 1);
+		ret = rtk_vlan_get(vid, (rtk_portmask_t *)&member_mask, 
+						(rtk_portmask_t *)&untag_mask, (rtk_fid_t *)&fid);
+
 		lua_pushnumber(L, ret);
-		lua_pushnumber(L, active);
+		lua_pushnumber(L, member_mask.bits[0]);
+		lua_pushnumber(L, untag_mask.bits[0]);
+		lua_pushnumber(L, fid);
+		
+		return 4;
+	}
+	else if (argc == 4) {
+		// set
+		vid = luaL_checkinteger(L, 1);
+		member_mask.bits[0] = luaL_checkinteger(L, 2);
+		untag_mask.bits[0] = luaL_checkinteger(L, 3);
+		fid = luaL_checkinteger(L, 4);
+		
+		ret = rtk_vlan_set(vid, member_mask, untag_mask, fid);
+		
+		lua_pushnumber(L, ret);
+		return 1;
+	}
+	else {
+		lua_pushnumber(L, RT_ERR_INPUT);
+		return 1;
+	}
+}
+
+// Lua: status[, pvid, priority] = rtl8370.vlan_portPvid(port[, pvid, priority])
+static int rtl8370_vlan_portPvid(lua_State* L)
+{
+	uint32_t argc = lua_gettop(L);
+	rtk_api_ret_t ret;
+	rtk_port_t port;
+	rtk_vlan_t pvid;
+	rtk_pri_t priority;
+
+	if (argc == 1) {
+		// get
+		port = luaL_checkinteger(L, 1);
+		ret = rtk_vlan_portPvid_get(port, (rtk_vlan_t *)&pvid, 
+						(rtk_pri_t *)&priority);
+
+		lua_pushnumber(L, ret);
+		lua_pushnumber(L, pvid);
+		lua_pushnumber(L, priority);
+		
+		return 3;
+	}
+	else if (argc == 3) {
+		// set
+		port = luaL_checkinteger(L, 1);
+		pvid = luaL_checkinteger(L, 2);
+		priority = luaL_checkinteger(L, 3);
+		
+		ret = rtk_vlan_portPvid_set(port, pvid, priority);
+		
+		lua_pushnumber(L, ret);
+		return 1;
+	}
+	else {
+		lua_pushnumber(L, RT_ERR_INPUT);
+		return 1;
+	}
+}
+
+// Lua: status[, igr_filter] = rtl8370.vlan_portIgrFilterEnable(port[, igr_filter])
+static int rtl8370_vlan_portIgrFilterEnable(lua_State* L)
+{
+	uint32_t argc = lua_gettop(L);
+	rtk_api_ret_t ret;
+	rtk_port_t port;
+	rtk_enable_t igr_filter;
+
+	if (argc == 1) {
+		// get
+		port = luaL_checkinteger(L, 1);
+		ret = rtk_vlan_portIgrFilterEnable_get(port, 
+						(rtk_enable_t *)&igr_filter);
+
+		lua_pushnumber(L, ret);
+		lua_pushnumber(L, igr_filter);
+
 		return 2;
 	}
-	else if (argc == 1) {
+	else if (argc == 2) {
 		// set
-		active = luaL_checkinteger(L, 1);
-		ret = rtk_led_serialMode_set(active);
+		port = luaL_checkinteger(L, 1);
+		igr_filter = luaL_checkinteger(L, 2);
+		
+		ret = rtk_vlan_portIgrFilterEnable_set(port, igr_filter);
+		
+		lua_pushnumber(L, ret);
+		return 1;
+	}
+	else {
+		lua_pushnumber(L, RT_ERR_INPUT);
+		return 1;
+	}
+}
+
+// Lua: status[, accept_frame_type] = rtl8370.vlan_portAcceptFrameType(port[, accept_frame_type])
+static int rtl8370_vlan_portAcceptFrameType(lua_State* L)
+{
+	uint32_t argc = lua_gettop(L);
+	rtk_api_ret_t ret;
+	rtk_port_t port;
+	rtk_vlan_acceptFrameType_t accept_frame_type;
+
+	if (argc == 1) {
+		// get
+		port = luaL_checkinteger(L, 1);
+		ret = rtk_vlan_portAcceptFrameType_get(port, 
+						(rtk_vlan_acceptFrameType_t *)&accept_frame_type);
+
+		lua_pushnumber(L, ret);
+		lua_pushnumber(L, accept_frame_type);
+
+		return 2;
+	}
+	else if (argc == 2) {
+		// set
+		port = luaL_checkinteger(L, 1);
+		accept_frame_type = luaL_checkinteger(L, 2);
+		
+		ret = rtk_vlan_portAcceptFrameType_set(port, accept_frame_type);
+		
+		lua_pushnumber(L, ret);
+		return 1;
+	}
+	else {
+		lua_pushnumber(L, RT_ERR_INPUT);
+		return 1;
+	}
+}
+
+// Lua: status[, priority] = rtl8370.vlan_vlanBasedPriority(vid[, priority])
+static int rtl8370_vlan_vlanBasedPriority(lua_State* L)
+{
+	uint32_t argc = lua_gettop(L);
+	rtk_api_ret_t ret;
+	rtk_vlan_t vid;
+	rtk_pri_t priority;
+
+	if (argc == 1) {
+		// get
+		vid = luaL_checkinteger(L, 1);
+		ret = rtk_vlan_vlanBasedPriority_get(vid, 
+						(rtk_pri_t *)&priority);
+
+		lua_pushnumber(L, ret);
+		lua_pushnumber(L, priority);
+
+		return 2;
+	}
+	else if (argc == 2) {
+		// set
+		vid = luaL_checkinteger(L, 1);
+		priority = luaL_checkinteger(L, 2);
+		
+		ret = rtk_vlan_vlanBasedPriority_set(vid, priority);
+		
+		lua_pushnumber(L, ret);
+		return 1;
+	}
+	else {
+		lua_pushnumber(L, RT_ERR_INPUT);
+		return 1;
+	}
+}
+
+// Lua: status[, proto_type, frame_type, cvid, cpri] = rtl8370.vlan_protoAndPortBasedVlan(operation, port[, proto_type, frame_type][, cvid, cpri])
+static int rtl8370_vlan_protoAndPortBasedVlan(lua_State* L)
+{
+	uint32_t argc = lua_gettop(L);
+	rtk_api_ret_t ret;
+	
+	rtk_port_t port;
+	rtk_vlan_protoAndPortInfo_t info;
+	
+	rtk_vlan_proto_type_t proto_type;
+	rtk_vlan_protoVlan_frameType_t frame_type;
+	
+	const char * operation;
+	size_t operation_length;
+
+	operation = lua_tolstring(L, 1, &operation_length);
+
+	if (argc == 4 && (!strcmp(operation, "get"))) {
+		// get
+		port = luaL_checkinteger(L, 2);
+		proto_type = luaL_checkinteger(L, 3);
+		frame_type = luaL_checkinteger(L, 4);
+		
+		ret = rtk_vlan_protoAndPortBasedVlan_get(port, proto_type,
+						frame_type, (rtk_vlan_protoAndPortInfo_t *)&info);
+
+		lua_pushnumber(L, ret);
+		lua_pushnumber(L, info.proto_type);
+		lua_pushnumber(L, info.frame_type);
+		lua_pushnumber(L, info.cvid);
+		lua_pushnumber(L, info.cpri);
+
+		return 5;
+	}
+	else if (argc == 4 && (!strcmp(operation, "del"))) {
+		// del
+		port = luaL_checkinteger(L, 2);
+		proto_type = luaL_checkinteger(L, 3);
+		frame_type = luaL_checkinteger(L, 4);
+		
+		ret = rtk_vlan_protoAndPortBasedVlan_del(port, proto_type, frame_type);
+
+		lua_pushnumber(L, ret);
+
+		return 1;
+	}
+	else if (argc == 6 && (!strcmp(operation, "add"))) {
+		// add
+		port = luaL_checkinteger(L, 2);
+		info.proto_type = luaL_checkinteger(L, 3);
+		info.frame_type = luaL_checkinteger(L, 4);
+		info.cvid = luaL_checkinteger(L, 5);
+		info.cpri = luaL_checkinteger(L, 6);
+		
+		ret = rtk_vlan_protoAndPortBasedVlan_add(port, info);
+		
+		lua_pushnumber(L, ret);
+		return 1;
+	}
+	else if (argc == 2 && (!strcmp(operation, "delAll"))) {
+		// delAll
+		port = luaL_checkinteger(L, 2);
+		
+		ret = rtk_vlan_protoAndPortBasedVlan_delAll(port);
+		
+		lua_pushnumber(L, ret);
+		return 1;
+	}
+	else {
+		lua_pushnumber(L, RT_ERR_INPUT);
+		return 1;
+	}
+}
+
+// Lua: status[, enable, fid] = rtl8370.vlan_portFid(port[, enable, fid])
+static int rtl8370_vlan_portFid(lua_State* L)
+{
+	uint32_t argc = lua_gettop(L);
+	rtk_api_ret_t ret;
+	rtk_port_t port;
+	rtk_enable_t enable;
+	rtk_fid_t fid;
+
+	if (argc == 1) {
+		// get
+		port = luaL_checkinteger(L, 1);
+		ret = rtk_vlan_portFid_get(port, (rtk_enable_t *)&enable, 
+						(rtk_fid_t *)&fid);
+
+		lua_pushnumber(L, ret);
+		lua_pushnumber(L, enable);
+		lua_pushnumber(L, fid);
+		
+		return 3;
+	}
+	else if (argc == 3) {
+		// set
+		port = luaL_checkinteger(L, 1);
+		enable = luaL_checkinteger(L, 2);
+		fid = luaL_checkinteger(L, 3);
+		
+		ret = rtk_vlan_portFid_set(port, enable, fid);
+		
 		lua_pushnumber(L, ret);
 		return 1;
 	}
@@ -322,57 +602,55 @@ static int rtl8370_vlan(lua_State* L)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Module function map
 static const LUA_REG_TYPE rtl8370_map[] = {
-	{ LSTRKEY( "init" ), LFUNCVAL( rtl8370_init )},
-	{ LSTRKEY( "smi" ), LFUNCVAL( rtl8370_smi )},
 	
-	// led
-	{ LSTRKEY( "led_serialMode" ), LFUNCVAL ( rtl8370_led_serialMode ) },
-	{ LSTRKEY( "led_groupConfig" ), LFUNCVAL ( rtl8370_led_groupConfig ) },
-	{ LSTRKEY( "led_blinkRate" ), LFUNCVAL ( rtl8370_led_blinkRate ) },
-	{ LSTRKEY( "led_modeForce" ), LFUNCVAL ( rtl8370_led_modeForce ) },
-	{ LSTRKEY( "led_mode" ), LFUNCVAL ( rtl8370_led_mode ) },
-	{ LSTRKEY( "led_operation" ), LFUNCVAL ( rtl8370_led_operation ) },
-	{ LSTRKEY( "led_enable" ), LFUNCVAL ( rtl8370_led_enable ) },
-
-	// Errors
-	{ LSTRKEY( "RT_ERR_OK" ), LNUMVAL( RT_ERR_OK ) },
-	{ LSTRKEY( "RT_ERR_FAILED" ), LNUMVAL( RT_ERR_FAILED ) },
-	{ LSTRKEY( "RT_ERR_SMI" ), LNUMVAL( RT_ERR_SMI ) },
-	{ LSTRKEY( "RT_ERR_INPUT" ), LNUMVAL( RT_ERR_INPUT ) },
+	// device functions
+	{ LSTRKEY( "init" ), 						LFUNCVAL( rtl8370_init )},
+	{ LSTRKEY( "smi" ), 						LFUNCVAL( rtl8370_smi )},
+	{ LSTRKEY( "hardreset" ), 					LFUNCVAL( rtl8370_hardreset )},
+	{ LSTRKEY( "softreset" ), 					LFUNCVAL( rtl8370_softreset )},
+	
+	// led functions
+	{ LSTRKEY( "led_serialMode" ), 				LFUNCVAL ( rtl8370_led_serialMode ) },
+	{ LSTRKEY( "led_groupConfig" ), 			LFUNCVAL ( rtl8370_led_groupConfig ) },
+	{ LSTRKEY( "led_blinkRate" ), 				LFUNCVAL ( rtl8370_led_blinkRate ) },
+	{ LSTRKEY( "led_modeForce" ), 				LFUNCVAL ( rtl8370_led_modeForce ) },
+	{ LSTRKEY( "led_mode" ), 					LFUNCVAL ( rtl8370_led_mode ) },
+	{ LSTRKEY( "led_operation" ), 				LFUNCVAL ( rtl8370_led_operation ) },
+	{ LSTRKEY( "led_enable" ), 					LFUNCVAL ( rtl8370_led_enable ) },
+	
+	// vlan functions
+	{ LSTRKEY( "vlan_init" ), 					LFUNCVAL( rtl8370_vlan_init )},
+	{ LSTRKEY( "vlan" ), 						LFUNCVAL( rtl8370_vlan )},
+	{ LSTRKEY( "vlan_portPvid" ), 				LFUNCVAL( rtl8370_vlan_portPvid )},
+	{ LSTRKEY( "vlan_portIgrFilterEnable" ), 	LFUNCVAL( rtl8370_vlan_portIgrFilterEnable )},
+	{ LSTRKEY( "vlan_portAcceptFrameType" ), 	LFUNCVAL( rtl8370_vlan_portAcceptFrameType )},
+	{ LSTRKEY( "vlan_vlanBasedPriority" ), 		LFUNCVAL( rtl8370_vlan_vlanBasedPriority )},
+	{ LSTRKEY( "vlan_protoAndPortBasedVlan" ), 	LFUNCVAL( rtl8370_vlan_protoAndPortBasedVlan )},
+	{ LSTRKEY( "vlan_portFid" ), 				LFUNCVAL( rtl8370_vlan_portFid )},
+	
+	
+	// Return numbers
+	{ LSTRKEY( "RT_ERR_OK" ), 					LNUMVAL( RT_ERR_OK ) },
+	{ LSTRKEY( "RT_ERR_FAILED" ), 				LNUMVAL( RT_ERR_FAILED ) },
+	{ LSTRKEY( "RT_ERR_SMI" ), 					LNUMVAL( RT_ERR_SMI ) },
+	{ LSTRKEY( "RT_ERR_INPUT" ), 				LNUMVAL( RT_ERR_INPUT ) },
+	{ LSTRKEY( "RT_ERR_PORT_ID" ), 				LNUMVAL( RT_ERR_PORT_ID ) },
+	{ LSTRKEY( "RT_ERR_VLAN_ACCEPT_FRAME_TYPE" ), LNUMVAL( RT_ERR_VLAN_ACCEPT_FRAME_TYPE ) },
+	{ LSTRKEY( "RT_ERR_VLAN_VID" ), 			LNUMVAL( RT_ERR_VLAN_VID ) },
+	{ LSTRKEY( "RT_ERR_VLAN_PRIORITY" ), 		LNUMVAL( RT_ERR_VLAN_PRIORITY ) },
+	{ LSTRKEY( "RT_ERR_TBL_FULL" ), 			LNUMVAL( RT_ERR_TBL_FULL ) },
+	{ LSTRKEY( "RT_ERR_OUT_OF_RANGE" ), 		LNUMVAL( RT_ERR_OUT_OF_RANGE ) },
+	{ LSTRKEY( "RT_ERR_INPUT" ), 				LNUMVAL( RT_ERR_INPUT ) },
+	
+	// end
 	{ LNILKEY, LNILVAL}
 };
 
 int luaopen_rtl8370(lua_State *L)
 {
-
+	// 
 
 }
 
